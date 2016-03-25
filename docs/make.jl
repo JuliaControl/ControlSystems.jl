96764c6dea18dcfa1f2978f5f876b9948ba40eb1
Mattias Fält <faldt.mattias@gmail.com>
2016-03-25 13:42:29 +0100

    Trying to not install pyplot when not deploying docs

--- docs/make.jl
+++ docs/make.jl
@@ -7,5 +7,15 @@ include("src/makeplots.jl")
 deploydocs(
     repo = "github.com/JuliaControl/ControlSystems.jl.git",
     latest = "master",
-    julia = "0.4"
+    julia = "0.4",
+    deps = myDeps()
 )
+
+function myDeps()
+    if get(ENV, "TRAVIS", "") != ""
+        run(`pip install --user pygments mkdocs`)
+        ENV["PYTHON"]=""
+        Pkg.add("PyPlot")
+        Pkg.build("PyCall")
+    end
+end
