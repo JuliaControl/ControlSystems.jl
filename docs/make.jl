91ab100f90c54ee09799d17376efb0bf09bb09a8
Mattias Fält <faldt.mattias@gmail.com>
2016-03-26 14:29:22 +0100

    Fixed when travis is generating plots for docs

--- docs/make.jl
+++ docs/make.jl
@@ -1,8 +1,24 @@
 using Lapidary, ControlSystems, Plots
+include("src/makeplots.jl")
 
 makedocs(modules=[ControlSystems])
 
-include("src/makeplots.jl")
+# If not running travis, generate the plots here, even if we are not deploying
+if get(ENV, "TRAVIS", "") == ""
+    makePlots()
+end
+
+# Only build plots in travis if we are deploying
+# And dont install the dependencies unless we are deploying
+function myDeps()
+    if get(ENV, "TRAVIS", "") != ""
+        run(`pip install --user pygments mkdocs`)
+        ENV["PYTHON"]=""
+        Pkg.add("PyPlot")
+        Pkg.build("PyCall")
+        makePlots()
+    end
+end
 
 deploydocs(
     repo = "github.com/JuliaControl/ControlSystems.jl.git",
@@ -10,12 +26,3 @@ deploydocs(
     julia = "0.4",
     deps = myDeps()
 )
-
-function myDeps()
-    if get(ENV, "TRAVIS", "") != ""
-        run(`pip install --user pygments mkdocs`)
-        ENV["PYTHON"]=""
-        Pkg.add("PyPlot")
-        Pkg.build("PyCall")
-    end
-end
