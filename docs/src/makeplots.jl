bedf3152254d7bed1232de0c9cae34dd7be19a27
Fredrik Bagge Carlsson <cont-frb@ulund.org>
2016-04-26 08:18:04 +0200

    added lqr design example with plot

--- docs/src/makeplots.jl
+++ docs/src/makeplots.jl
@@ -6,6 +6,22 @@ plotsDir = (pwd()[end-3:end] == "docs") ? "build/plots" : "docs/build/plots"
 mkdir(plotsDir)
 Plots.pyplot()
 
+# LQG design
+h = 0.1
+A = [1 h; 0 1]
+B = [0;1]
+C = [1 0]
+sys = ss(A,B,C,0, h)
+Q = eye(2)
+R = eye(1)
+L = dlqr(A,B,Q,R) # lqr(sys,Q,R) can also be used
+
+u(t,x) = -L*x # Form control law
+t=0:h:5
+x0 = [1,0]
+y, t, x, uout = lsim(sys,u,t,x0)
+plot(t,x, lab=["Position", "Velocity"]', xlabel="Time [s]")
+Plots.savefig(plotsDir*"/lqrplot.svg")
 
 # PID design functions
 P = tf(1,[1,1])^4
