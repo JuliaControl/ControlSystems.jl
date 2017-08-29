49837b9350f520b1bfbac35b46e8ba9e83a3b4f9
Fredrik Bagge Carlson <cont-frb@ulund.org>
2017-08-29 07:51:32 +0200

    Small improvements according to review comments

--- test/test_simplification.jl
+++ test/test_simplification.jl
@@ -21,6 +21,6 @@ sysmin = minreal(sys)
 t = 0:0.1:10
 y1,x1 = step(sys,t)[[1,3]]
 y2,x2 = step(sysmin,t)[[1,3]]
-@test_approx_zero sum(y1.-y2) # Test that the output from the two systems are the same
+@test_approx_zero norm(y1.-y2) # Test that the output from the two systems are the same
 
 end
