877e71b42d5d8b20edfe54157cd9fb9ee9f2fb18
Albin Heimerson <albin.heimerson@control.lth.se>
2019-05-07 10:33:31 +0200

    fix broken test

--- test/test_autovec.jl
+++ test/test_autovec.jl
@@ -1,12 +1,11 @@
 @testset "test_autovec" begin
 
-# Test creating simple function and check output
-ControlSystems.@autovec (1, 3) 4 f() = (ones(3, 1, 1), ones(3), ones(1, 3), ones(3))
-@test f() == (ones(3, 1, 1), ones(3), ones(1, 3), ones(3))
-@test fv() == (ones(3), ones(3), ones(3), ones(3))
+# Check output of bode and make sure dimensions are correct
+sys = tf([1], [1, 2])
+mag, phase, w = bode(sys)
+@test size(mag) == size(phase) == (size(w)[1], 1, 1)
 
 # Check output of bodev and make sure dimensions are correct
-sys = tf([1], [1, 2])
 mag, phase, w = bodev(sys)
 @test size(mag) == size(phase) == size(w)
 
