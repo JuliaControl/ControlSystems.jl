f3cbd892b6ebdfcc7b7e8a32979dcca39b8c8532
Fredrik Bagge Carlson <baggepinnen@gmail.com>
2019-05-08 10:29:04 +0200

    Update test/test_autovec.jl
    Co-Authored-By: albheim <albheim@hotmail.com>

--- test/test_autovec.jl
+++ test/test_autovec.jl
@@ -3,7 +3,7 @@
 # Check output of bode and make sure dimensions are correct
 sys = tf([1], [1, 2])
 mag, phase, w = bode(sys)
-@test size(mag) == size(phase) == (size(w)[1], 1, 1)
+@test size(mag) == size(phase) == (size(w,1), 1, 1)
 
 # Check output of bodev and make sure dimensions are correct
 mag, phase, w = bodev(sys)
