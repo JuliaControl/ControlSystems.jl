c2ea9253dab3cc3145c0e31ebc4e318963f5bf90
Fredrik Bagge Carlson <cont-frb@ulund.org>
2019-05-12 13:43:48 +0200

    add some tests for autovec

--- test/test_autovec.jl
+++ test/test_autovec.jl
@@ -9,4 +9,39 @@ mag, phase, w = bode(sys)
 mag, phase, w = bodev(sys)
 @test size(mag) == size(phase) == size(w)
 
+w = exp10.(LinRange(-3,3,10))
+mag, phase, _ = bodev(sys, w)
+@test size(mag) == size(phase) == size(w)
+
+# Test that we can define varous kinds of methods with autovec
+
+# Test keyword arguments
+ControlSystems.@autovec (2,) 2 function t1(; kw=1)
+    return kw, [kw kw]
+end
+
+@test @isdefined t1
+@test @isdefined t1v
+@test t1() == (1,[1 1])
+@test t1v() == (1,[1, 1])
+
+@test t1(kw=2) == (2,[2 2])
+@test t1v(kw=2) == (2,[2, 2])
+
+# Test type constraints on keyword arguments
+ControlSystems.@autovec (2,) 2 function t2(; kw::T=1) where {T <: Integer}
+    return kw, [kw kw]
+end
+
+@test @isdefined t2
+@test @isdefined t2v
+@test t2() == (1,[1 1])
+@test t2v() == (1,[1, 1])
+
+@test t2(kw=2) == (2,[2 2])
+@test t2v(kw=2) == (2,[2, 2])
+
+@test_throws MethodError t2(kw=2.) # This method should ont be defined
+
+
 end
