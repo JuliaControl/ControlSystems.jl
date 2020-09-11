a466f857d0045a7f91598a5a8b880a3b7038e5e1
olof3 <olof3@users.noreply.github.com>
2020-09-11 16:10:21 +0200

    Removed Polynomials v0.8 from deps since it has problematic type instability.

--- test/runtests.jl
+++ test/runtests.jl
@@ -10,8 +10,7 @@ eye_(n) = Matrix{Int64}(I, n, n)
 
 my_tests = [
             "test_statespace",
-            "test_transferfunction",
-            "test_delayed_systems",
+            "test_transferfunction",            
             "test_zpk",
             "test_promotion",
             "test_connections",
@@ -27,6 +26,7 @@ my_tests = [
             "test_lqg",
             "test_synthesis",
             "test_partitioned_statespace",
+            "test_delayed_systems",
             "test_demo_systems",
             ]
 
