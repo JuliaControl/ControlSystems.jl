094e3cc602547022cdea5e87d51f1d71574a56fa
Fredrik Bagge Carlson <baggepinnen@gmail.com>
2020-10-21 10:04:05 +0200

    Improve docstring of balance (#374)

--- src/matrix_comps.jl
+++ src/matrix_comps.jl
@@ -452,11 +452,13 @@ function _infnorm_two_steps_dt(sys::AbstractStateSpace, normtype::Symbol, tol=1e
 end
 
 
-"""`S, P, B = balance(A[, perm=true])`
+"""
+    S, P, B = balance(A[, perm=true])
 
-Compute a similarity transform `T` resulting in `B = T\\A*T` such that the row
+Compute a similarity transform `T = S*P` resulting in `B = T\\A*T` such that the row
 and column norms of `B` are approximately equivalent. If `perm=false`, the
-transformation will only scale `A` using diagonal `S`, and not permute `A` (i.e., set `P=I`)."""
+transformation will only scale `A` using diagonal `S`, and not permute `A` (i.e., set `P=I`).
+"""
 function balance(A, perm::Bool=true)
     n = LinearAlgebra.checksquare(A)
     B = copy(A)
