8d03b6f86499fa2cdfa23d1c5b068c877f60a924
Albin Heimerson <albin.heimerson@control.lth.se>
2020-11-04 00:27:23 +0100

    Update src/types/SisoTfTypes/SisoZpk.jl
    Co-authored-by: olof3 <olof3@users.noreply.github.com>

--- src/types/SisoTfTypes/SisoZpk.jl
+++ src/types/SisoTfTypes/SisoZpk.jl
@@ -104,7 +104,7 @@ function minreal(sys::SisoZpk{T,TR}, eps::Real) where {T, TR}
 end
 
 """Reorder the vector x of complex numbers so that complex conjugates come after each other, with the one with positive imaginary part first. Returns true if the conjugates can be paired and otherwise false."""
-function pairup_conjugates!(x)
+function pairup_conjugates!(x::AbstractVector)
     i = 0
     while i < length(x)
         i += 1
