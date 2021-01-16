5cdc908e32d1bdf332086e50ad4370e5debf1cc5
Fredrik Bagge Carlson <baggepinnen@gmail.com>
2021-01-16 09:13:00 +0100

    add resonant measurement distrubance

--- src/model_augmentation.jl
+++ src/model_augmentation.jl
@@ -1,7 +1,7 @@
 """
     add_disturbance(sys::AbstractStateSpace{Continuous}, Ad::AbstractMatrix, Cd::AbstractMatrix)
 
-See CSS pp. 144
+See CCS pp. 144
 
 # Arguments:
 - `sys`: System to augment
@@ -21,6 +21,17 @@ function add_disturbance(sys::AbstractStateSpace{Continuous}, Ad::AbstractMatrix
     ss(Ae,Be,Ce,De)
 end
 
+function add_measurement_disturbance(sys::AbstractStateSpace{Continuous}, Ad::AbstractMatrix, Cd::AbstractMatrix)
+    A,B,C,D = ControlSystems.ssdata(sys)
+    T = eltype(A)
+    nx,nu,ny = sys.nx,sys.nu,sys.ny
+    Ae = [A zeros(T, nx, size(Ad, 1)); zeros(T, size(Ad, 1), nx) Ad]
+    Be = [B; zeros(T, size(Ad, 1), nu)]
+    Ce = [C Cd]
+    De = D
+    ss(Ae,Be,Ce,De)
+end
+
 function add_low_frequency_disturbance(sys::AbstractStateSpace{Continuous}, Ai::Integer; ϵ=0)
     nx,nu,ny = sys.nx,sys.nu,sys.ny
     Cd = zeros(nx, 1)
@@ -33,12 +44,17 @@ function add_low_frequency_disturbance(sys::AbstractStateSpace{Continuous}; ϵ=0
     add_disturbance(sys, fill(-ϵ, 1, 1), Cd)
 end
 
-function add_resonant_disturbance(sys::AbstractStateSpace{Continuous}, ω, ζ, Ai::Integer)
+function add_resonant_disturbance(sys::AbstractStateSpace{Continuous}, ω, ζ, Ai::Integer; measurement=false)
     nx,nu,ny = sys.nx,sys.nu,sys.ny
-    Cd = zeros(nx, 2)
-    Cd[Ai, 1] = 1
+    if measurement
+        Cd = zeros(ny, 2)
+        Cd[Ai, 1] = 1
+    else
+        Cd = zeros(nx, 2)
+        Cd[Ai, 1] = 1
+    end
     Ad = [-ζ -ω; ω -ζ]
-    add_disturbance(sys, Ad, Cd)
+    measurement ? add_measurement_disturbance(sys, Ad, Cd) : add_disturbance(sys, Ad, Cd)
 end
 
 
