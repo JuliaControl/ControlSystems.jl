7b4f5b5d505f8ab398832a9618f50cbc8da134d2
Albin Heimerson <albin.heimerson@control.lth.se>
2021-02-15 10:38:23 +0100

    maintain zpk type in c2d

--- src/discrete.jl
+++ src/discrete.jl
@@ -227,12 +227,11 @@ function c2d_poly2poly(p,h)
 end
 
 
-function c2d(G::TransferFunction{<:Continuous}, h, args...; kwargs...)
-    ny, nu = size(G)
-    @assert (ny + nu == 2) "c2d(G::TransferFunction, h) not implemented for MIMO systems"
+function c2d(G::TransferFunction{TE, T}, h, args...; kwargs...) where {TE<:Continuous, T<:SisoTf}
+    @assert issiso(G) "c2d(G::TransferFunction, h) not implemented for MIMO systems"
     sys = ss(G)
     sysd = c2d(sys, h, args...; kwargs...)
-    return convert(TransferFunction, sysd)
+    return convert(TransferFunction{typeof(Discrete(h)), T}, sysd)
 end
 
 """
