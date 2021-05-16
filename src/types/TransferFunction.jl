1c3cbb7c272abc526ba40ce744fc1a35e947619c
Albin Heimerson <albin.heimerson@control.lth.se>
2021-05-16 11:33:34 +0200

    Update src/types/TransferFunction.jl

--- src/types/TransferFunction.jl
+++ src/types/TransferFunction.jl
@@ -24,7 +24,6 @@ end
 
 noutputs(G::TransferFunction) = size(G.matrix, 1)
 ninputs(G::TransferFunction) = size(G.matrix, 2)
-nstates(G::TransferFunction) = length(pole(G)) # Does not work for MIMO 
 
 #####################################################################
 ##                          Misc. Functions                        ##
