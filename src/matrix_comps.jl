420f086d97e63cbcfdc41b6cadf46a5baf62faf5
Fredrik Bagge Carlson <baggepinnen@gmail.com>
2021-05-24 05:48:06 +0200

    Update src/matrix_comps.jl
    Co-authored-by: olof3 <olof3@users.noreply.github.com>

--- src/matrix_comps.jl
+++ src/matrix_comps.jl
@@ -102,7 +102,7 @@ end
     obsv(A, C, n=size(A,1))
     obsv(sys, n=sys.nx)
 
-Compute the observability matrix with `n` rows for the system described by `(A, C)` or `sys`. Providingthe optional `n > sys.nx` returns an extended observability matrix.
+Compute the observability matrix with `n` rows for the system described by `(A, C)` or `sys`. Providing the optional `n > sys.nx` returns an extended observability matrix.
 
 Note that checking for observability by computing the rank from `obsv` is
 not the most numerically accurate way, a better method is checking if
