bc662638cac69100ca141a5f3e8f9736d9d7b179
Albin Heimerson <albin.heimerson@control.lth.se>
2021-06-03 11:49:34 +0200

    Fix pid creation, update stabregionpid

--- test/test_pid_design.jl
+++ test/test_pid_design.jl
@@ -1,5 +1,5 @@
 @testset "test_pid_design" begin
-@test stabregionPID(s->exp(-sqrt(s)), exp10.(range(-3, stop=3, length=50)), kd=1)[2][1] ≈ -1.022356911142034
+@test stabregionPID(s->exp(-sqrt(s)), exp10.(range(-3, stop=3, length=50)); kd=1, form=:parallel)[2][:Kp][1] ≈ -1.022356911142034
 
 P = tf(1,[1,1])^4
 gangoffourplot(P,tf(1))
@@ -9,10 +9,10 @@ C, params = loopshapingPI(P, ωp, phasemargin=60, form=:parallel, doplot=true)
 @test params.Kp ≈ 0.82274734724854
 @test params.Ki ≈ -0.45472580052281375
 
-C = pid(TransferFunction; Kp = 1, Ki = 1, Kd=1) 
+C = pid(TransferFunction; Kp=1.0, Ki=1, Kd=1) 
 @test C == tf(1) + tf(1,[1,0]) + tf([1,0],[1])
-pidplots(C, :nyquist,:gof,:pz,:controller, grid=true) # Simply test that the functions runs and not errors
-pidplots(C, :nyquist,:gof,:pz,:controller, grid=false)
+pidplots(C, :nyquist, :gof, :pz, :controller; Kp=[1.0, 2], Ki=[2, 3], grid=true) # Simply test that the functions runs and not errors
+pidplots(C, :nyquist, :gof, :pz, :controller; Kp=1.0, Ki=[2, 3], grid=false)
 leadlinkcurve()
 
 stabregionPID(tf(1,[1,0]))
@@ -28,8 +28,8 @@ C, params = placePI(P, 2, 0.7)
 @test pole(feedback(P, C)) ≈ [-1.4 + √2.04im, -1.4 - √2.04im]
 @test [params.Kp, params.Ti] ≈ [9/5, 9/20]
 
-@test values(ControlSystems.convert_pid_params(:standard; Kp=2, Ki=3, Kd=4)) ≈ [2, 2/3, 2]
-@test values(ControlSystems.convert_pid_params(:series; Kp=2, Ti=4, Td=1)) ≈ [1, 2, 2]
-@test values(ControlSystems.convert_pid_params(:parallel; Kc=2, τi=3, τd=4)) ≈ [14/3, 2/3, 8]
+@test all(values(ControlSystems.convert_pid_params(:standard; Kp=2.0, Ki=3, Kd=4)) .≈ (2, 2/3, 2))
+@test all(values(ControlSystems.convert_pid_params(:series; Kp=2.0, Ti=4, Td=1)) .≈ (1, 2, 2))
+@test all(values(ControlSystems.convert_pid_params(:parallel; Kc=2.0, τi=3, τd=4)) .≈ (14/3, 2/3, 8))
 
 end
