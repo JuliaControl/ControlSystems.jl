597183d8cdec0663615cb5b94f2af984581a330e
Albin Heimerson <albin.heimerson@control.lth.se>
2021-10-14 16:21:29 +0200

    revert some code and make stuff work

--- test/test_pid_design.jl
+++ test/test_pid_design.jl
@@ -1,35 +1,39 @@
 @testset "test_pid_design" begin
-@test stabregionPID(s->exp(-sqrt(s)), exp10.(range(-3, stop=3, length=50)); kd=1, form=:parallel)[2][:Kp][1] ≈ -1.022356911142034
+@test stabregionPID(s->exp(-sqrt(s)), exp10.(range(-3, stop=3, length=50)); kd=1)[2][1][1] ≈ -1.022356911142034
 
 P = tf(1,[1,1])^4
 gangoffourplot(P,tf(1))
 gangoffourplot(ss(P),ss(1))
 ωp = 0.8
-C, params = loopshapingPI(P, ωp, phasemargin=60, form=:parallel, doplot=true)
-@test params.Kp ≈ 0.82274734724854
-@test params.Ki ≈ -0.45472580052281375
+C, kp, ki = loopshapingPI(P, ωp, phasemargin=60, form=:parallel, doplot=true)
+@test kp ≈ 0.82274734724854
+@test ki ≈ -0.45472580052281375
 
-C = pid(TransferFunction; Kp=1.0, Ki=1, Kd=1) 
+C = pid(1.0, 1, 1) 
 @test C == tf(1) + tf(1,[1,0]) + tf([1,0],[1])
-pidplots(C, :nyquist, :gof, :pz, :controller; Kp=[1.0, 2], Ki=[2, 3], grid=true) # Simply test that the functions runs and not errors
-pidplots(C, :nyquist, :gof, :pz, :controller; Kp=1.0, Ki=[2, 3], grid=false)
+pidplots(C, :nyquist, :gof, :pz, :controller; kps=[1.0, 2], kis=[2, 3], grid=true) # Simply test that the functions runs and not errors
+pidplots(C, :nyquist, :gof, :pz, :controller; kps=1.0, kis=[2, 3], grid=false)
 leadlinkcurve()
 
 stabregionPID(tf(1,[1,0]))
 stabregionPID(s -> exp(-sqrt(s)))
 
 P = tf(1,[1, 1, 1])
-C, params = loopshapingPI(P,10; phasemargin = 30, doplot = false)
+C, kp, ki = loopshapingPI(P,10; phasemargin = 30, doplot = false)
 _,_,_,pm = margin(P*C)
 @test pm[] > 30
 
 P = tf(1,[1, 1])
-C, params = placePI(P, 2, 0.7)
+C, kp, ki = placePI(P, 2, 0.7)
 @test pole(feedback(P, C)) ≈ [-1.4 + √2.04im, -1.4 - √2.04im]
-@test [params.Kp, params.Ti] ≈ [9/5, 9/20]
+@test [kp, ki] ≈ [9/5, 9/20]
 
-@test all(values(ControlSystems.convert_pid_params(:standard; Kp=2.0, Ki=3, Kd=4)) .≈ (2, 2/3, 2))
-@test all(values(ControlSystems.convert_pid_params(:series; Kp=2.0, Ti=4, Td=1)) .≈ (1, 2, 2))
-@test all(values(ControlSystems.convert_pid_params(:parallel; Kc=2.0, τi=3, τd=4)) .≈ (14/3, 2/3, 8))
+params = (2, 3, 0.5)
+parallel_params = ControlSystems.convert_pidparams_from_standard(params..., :parallel)
+@test parallel_params == (2, 2/3, 1)
+@test ControlSystems.convert_pidparams_to_standard(parallel_params..., :parallel) == params
+series_params = ControlSystems.convert_pidparams_from_standard(params..., :series)
+@test series_params == ((3-sqrt(3))/3, (3-sqrt(3))/2, (3+sqrt(3))/2)
+@test ControlSystems.convert_pidparams_to_standard(series_params..., :series) == params
 
 end
