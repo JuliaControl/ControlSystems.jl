2d4eb57d1fea54f12532efc83f1a310462b5b8e9
Fredrik Bagge Carlson <baggepinnen@gmail.com>
2022-01-25 07:09:41 +0100

    export balance_statespace
    - add test
    - fix docstring

--- src/types/conversion.jl
+++ src/types/conversion.jl
@@ -156,15 +156,16 @@ function siso_tf_to_ss(T::Type, f::SisoRational)
 end
 
 """
-`A, B, C, T = balance_statespace{S}(A::Matrix{S}, B::Matrix{S}, C::Matrix{S}, perm::Bool=false)`
-
-`sys, T = balance_statespace(sys::StateSpace, perm::Bool=false)`
+    A, B, C, T = balance_statespace{S}(A::Matrix{S}, B::Matrix{S}, C::Matrix{S}, perm::Bool=false)
+    sys, T = balance_statespace(sys::StateSpace, perm::Bool=false)
 
 Computes a balancing transformation `T` that attempts to scale the system so
 that the row and column norms of [T*A/T T*B; C/T 0] are approximately equal.
 If `perm=true`, the states in `A` are allowed to be reordered.
 
-This is not the same as finding a balanced realization with equal and diagonal observability and reachability gramians, see `balreal`
+The inverse of `sysb, T = balance_statespace(sys)` is given by `similarity_transform(sysb, T)`
+
+This is not the same as finding a balanced realization with equal and diagonal observability and reachability gramians, see [`balreal`](@ref)
 """
 function balance_statespace(A::AbstractMatrix, B::AbstractMatrix, C::AbstractMatrix, perm::Bool=false)
     try
