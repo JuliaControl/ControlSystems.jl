9455a541cc9f04bb201eccbd9d29f10d51ff8eae
Fredrik Bagge Carlson <baggepinnen@gmail.com>
2022-01-28 16:32:58 +0100

    remove prescale (#620)

--- src/matrix_comps.jl
+++ src/matrix_comps.jl
@@ -599,27 +599,6 @@ function similarity_transform(sys::ST, T; unitary=false) where ST <: AbstractSta
     ST(A,B,C,D,sys.timeevol)
 end
 
-"""
-    syst, S = prescale(sys)
-Perform a eigendecomposition on system state-transition matrix `sys.A`.
-```
-Ã = S⁻¹AS
-B̃ = S⁻¹ B
-C̃ = CS
-D̃ = D
-```
-Such that `Ã` is diagonal.
-Returns a new scaled state-space object and the associated transformation
-matrix.
-"""
-function prescale(sys::AbstractStateSpace)
-    d, S = eigen(sys.A)
-    A = Diagonal(d)
-    B = S\sys.B
-    C = sys.C*S
-    normalized_sys = iscontinuous(sys) ? ss(A, B, C, sys.D) : ss(A, B, C, sys.D, sys.Ts)
-    return normalized_sys, S
-end
 
 """
     sysi = innovation_form(sys, R1, R2)
