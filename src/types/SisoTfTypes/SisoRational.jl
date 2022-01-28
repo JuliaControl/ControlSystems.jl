841a387fd8132071e0bc115f295c2aa9bbe9cc36
Albin Heimerson <albin.heimerson@control.lth.se>
2022-01-28 13:45:50 +0100

    Allow compare between poly of different lengths

--- src/types/SisoTfTypes/SisoRational.jl
+++ src/types/SisoTfTypes/SisoRational.jl
@@ -101,6 +101,9 @@ function isapprox(f1::SisoRational, f2::SisoRational; rtol::Real=sqrt(eps()), at
     # Get representation of num/den so index access is correct
     f1num, f1den = numvec(f1), denvec(f1)
     f2num, f2den = numvec(f2), denvec(f2)
+    _prefill_zero(x, n) = [zeros(n - length(x)); x]
+    f1num, f2num = _prefill_zero.((f1num, f2num), maximum(length.((f1num, f2num))))
+    f1den, f2den = _prefill_zero.((f1den, f2den), maximum(length.((f1den, f2den))))
     isapprox(f1num * f2den[1], f2num * f1den[1], rtol=rtol, atol=atol) && isapprox(f1den * f2den[1], f2den * f1den[1], rtol=rtol, atol=atol)
 end
 
