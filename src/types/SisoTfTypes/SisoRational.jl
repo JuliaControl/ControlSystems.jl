a9ad53de4854093422985d9d304b76d506f9893f
Albin Heimerson <albin.heimerson@control.lth.se>
2022-01-28 15:03:38 +0100

    Fix some problems for comparisons between tfs (#622)
    * Use den/num vec and fix indexing
    
    * Add tests that would have failed before
    
    * Allow compare between poly of different lengths
    
    * less allocations

--- src/types/SisoTfTypes/SisoRational.jl
+++ src/types/SisoTfTypes/SisoRational.jl
@@ -89,11 +89,29 @@ function evalfr(f::SisoRational{T}, s::Number) where T
     end
 end
 
-==(f1::SisoRational, f2::SisoRational) = (f1.num * f2.den[1] == f2.num * f1.den[1] && f1.den * f2.den[1] == f2.den * f1.den[1]) # NOTE: Not in analogy with how it's done for SisoZpk
+function ==(f1::SisoRational, f2::SisoRational)
+    # Get representation of num/den so index access is correct
+    f1num, f1den = numvec(f1), denvec(f1)
+    f2num, f2den = numvec(f2), denvec(f2)
+    (f1num * f2den[1] == f2num * f1den[1] && f1den * f2den[1] == f2den * f1den[1]) # NOTE: Not in analogy with how it's done for SisoZpk
+end
 
 # We might want to consider alowing scaled num and den as equal
 function isapprox(f1::SisoRational, f2::SisoRational; rtol::Real=sqrt(eps()), atol::Real=0)
-    isapprox(f1.num * f2.den[1], f2.num * f1.den[1], rtol=rtol, atol=atol) && isapprox(f1.den * f2.den[1], f2.den * f1.den[1], rtol=rtol, atol=atol)
+    # Get representation of num/den so index access is correct
+    f1num, f1den = numvec(f1), denvec(f1)
+    f2num, f2den = numvec(f2), denvec(f2)
+    if length(f1num) < length(f2num)
+        f1num = [zeros(length(f2num) - length(f1num)); f1num]
+    elseif length(f2num) < length(f1num)
+        f2num = [zeros(length(f1num) - length(f2num)); f2num]
+    end
+    if length(f1den) < length(f2den)
+        f1den = [zeros(length(f2den) - length(f1den)); f1den]
+    elseif length(f2den) < length(f1den)
+        f2den = [zeros(length(f1den) - length(f2den)); f2den]
+    end
+    isapprox(f1num * f2den[1], f2num * f1den[1], rtol=rtol, atol=atol) && isapprox(f1den * f2den[1], f2den * f1den[1], rtol=rtol, atol=atol)
 end
 
 +(f1::SisoRational, f2::SisoRational) = SisoRational(f1.num*f2.den + f2.num*f1.den, f1.den*f2.den)
