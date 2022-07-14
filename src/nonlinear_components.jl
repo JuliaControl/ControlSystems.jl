8497bbbe779c9de66141a022a1b3ce54c82ad836
Fredrik Bagge Carlson <baggepinnen@gmail.com>
2022-07-14 10:11:27 +0200

    add deadzone nonlinearity

--- src/nonlinear_components.jl
+++ src/nonlinear_components.jl
@@ -17,3 +17,48 @@ function ratelimit(args...; Tf)
     integrator*saturation(args...)*F
 end
 
+
+
+##
+
+struct DeadZone{T} <: Function
+    l::T
+    u::T
+end
+DeadZone(u) = DeadZone(-u, u)
+
+function (s::DeadZone)(x)
+    if x > s.u
+        x - s.u
+    elseif x < s.l
+        x - s.l
+    else
+        zero(x)
+    end
+end
+
+"""
+    deadzone(val)
+    deadzone(lower, upper)
+
+Create a dead-zone nonlinearity.
+
+```
+       y▲
+        │     /
+        │    /
+  lower │   /
+─────|──┼──|───────► u
+    /   │   upper
+   /    │
+  /     │
+```
+
+$nonlinear_warning
+
+Note: when composing linear systems with nonlinearities, it's often important to handle operating points correctly.
+See [`ControlSystems.offset`](@ref) for handling operating points.
+"""
+deadzone(args...) = nonlinearity(DeadZone(args...))
+deadzone(v::AbstractVector, args...) = nonlinearity(DeadZone.(v, args...))
+Base.show(io::IO, f::DeadZone) = f.u == -f.l ? print(io, "deadzone($(f.u))") : print(io, "deadzone($(f.l), $(f.u))")
