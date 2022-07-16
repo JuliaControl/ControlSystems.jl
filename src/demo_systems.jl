f83637fc350feaa363f9fe6b3350520826871d8b
Fredrik Bagge Carlson <baggepinnen@gmail.com>
2022-07-16 16:23:58 +0200

    add dmm to docstring

--- src/demo_systems.jl
+++ src/demo_systems.jl
@@ -40,6 +40,7 @@ SISO systems
 * `DemoSystems.fotd(;T=1, τ=1)`   First-order system with time delay `exp(-sτ)/(sT+1)`
 * `DemoSystems.sotd(;T=1, T2=10, τ=1)`   Second-order non-resonant system with time delay `exp(-sτ)/(sT+1)/(sT2 + 1)`
 * `DemoSystems.resonant(;ω0=1, ζ=0.25)`   Second-order resonant systems `ω0^2/(s^2 + 2ζ*ω0*s + ω0^2)`
+* `DemoSystems.double_mass_model` Fourth-order model of a flexible transmission. See docstrings for details.
 
 MIMO systems
 ============
