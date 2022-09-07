2c2316f9c9fe242aa3e4380836c9a2dc13bc7f68
Fredrik Bagge Carlson <baggepinnen@gmail.com>
2022-09-07 07:32:32 +0200

    Fix rlocus when poles cross in both real and imag
    Uses the Hungarian algorithm to find the optimal assignment between the new roots and the roots form the previous iteration.
    
    Closes #740

--- test/test_pid_design.jl
+++ test/test_pid_design.jl
@@ -178,6 +178,11 @@ wt = w[findmax(mag[:])[2]]
 @test wt ≈ ω  rtol=1e-2
 
 
+# https://github.com/JuliaControl/ControlSystems.jl/issues/740
+# Nroots = -1.0 .+  [-1.732050807568877im, 1.732050807568877im]
+# Droots =  [0.0, -4.0, -6.0, -0.7 - 0.7141428428542851im, -0.7 + 0.7141428428542851im]
+# G = zpk(Nroots, Droots, 1.0)
+# rlocusplot(G, 200)
 
 end
 
