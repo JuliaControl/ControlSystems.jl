5037595b5034dfada9493c6074980898a601a397
agerlach <599421+agerlach@users.noreply.github.com>
2022-09-07 08:46:43 -0400

    update rlocus docstring

--- src/pid_design.jl
+++ src/pid_design.jl
@@ -204,13 +204,11 @@ end
 
 
 """
-    rlocusplot(P::LTISystem, K)
+    rlocusplot(P::LTISystem; K)
 
 Computes and plots the root locus of the SISO LTISystem P with
-a negative feedback loop and feedback gains `K`, if `K` is not provided,
-range(1e-6,stop=500,length=10000) is used.
-If `OrdinaryDiffEq.jl` is installed and loaded by the user (`using OrdinaryDiffEq`), `rlocusplot` will use an adaptive step-size algorithm to
-select values of `K`. A scalar `Kmax` can then be given as second argument.
+a negative feedback loop and feedback gains between 0 and `K`. `rlocusplot` will use an adaptive step-size algorithm to
+determine the values of the feedback gains used to generate the plot.
 """
 rlocusplot
 @recipe function rlocusplot(p::Rlocusplot; K=500)
