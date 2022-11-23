75ef3afeb147e62326dc8c1a36c00620df7b0aad
Michele Zaffalon <michele.zaffalon@gmail.com>
2022-11-23 08:36:40 +0100

    Implement getpoles for a vector K passed as a parameter (#775)
    * Restrict size of input parameters
    
    * Implement getpoles for a given vector K
    
    * Undo add whitespace
    
    * Add test
    
    * Add root locus for k=0 in method root_locus with adaptive k
    
    Co-authored-by: Michele Zaffalon <michele.zaffalon@bruker.com>

--- src/root_locus.jl
+++ src/root_locus.jl
@@ -2,18 +2,18 @@ const Polynomials = ControlSystemsBase.Polynomials
 @userplot Rlocusplot
 
 
-function getpoles(G, K)
+function getpoles(G, K::Number)
     issiso(G) || error("root locus only supports SISO systems")
     G isa TransferFunction || (G = tf(G))
     P = numpoly(G)[]
     Q = denpoly(G)[]
-    T = float(eltype(K))
+    T = float(typeof(K))
     ϵ = eps(T)
     nx = length(Q)
     D = zeros(nx-1, nx-1) # distance matrix
     prevpoles = ComplexF64[]
     temppoles = zeros(ComplexF64, nx-1)
-    f = function (y,_,k)
+    f = function (_,_,k)
         if k == 0 && length(P) > length(Q)
             # More zeros than poles, make sure the vector of roots is of correct length when k = 0
             # When this happens, there are fewer poles for k = 0, these poles can be seen as beeing located somewhere at Inf
@@ -36,12 +36,36 @@ function getpoles(G, K)
     integrator = OrdinaryDiffEq.init(prob,OrdinaryDiffEq.Tsit5(),reltol=1e-8,abstol=1e-8)
     ts         = Vector{Float64}()
     poleout    = Vector{Vector{ComplexF64}}()
+    push!(poleout,integrator.k[1])
+    push!(ts,0)
     for i in integrator
-        push!(poleout,integrator.k[1])
+        push!(poleout,integrator.k[end])
         push!(ts,integrator.t[1])
     end
     poleout = hcat(poleout...)'
     poleout, ts
+end
+
+
+function getpoles(G, K::AbstractVector{T}) where {T<:Number}
+    issiso(G) || error("root locus only supports SISO systems")
+    G isa TransferFunction || (G = tf(G))
+    P, Q = numpoly(G)[], denpoly(G)[]
+    poleout = Matrix{ComplexF64}(undef, Polynomials.degree(Q), length(K))
+    nx = length(Q)
+    D = zeros(nx-1, nx-1) # distance matrix
+    temppoles = zeros(ComplexF64, nx-1)
+    for (i, k) in enumerate(K)
+        k == 0 && length(P) > length(Q) && (k = eps(T))
+        poleout[:,i] = ComplexF64.(Polynomials.roots(k[1]*P+Q))
+        if i > 1
+            D .= abs.(poleout[:,i] .- transpose(poleout[:,i-1]))
+            assignment, _ = Hungarian.hungarian(D)
+            foreach(k->temppoles[assignment[k]] = poleout[:,i][k], 1:nx-1)
+            poleout[:,i] .= temppoles
+        end
+    end
+    copy(poleout'), K
 end
 
 
@@ -53,7 +77,6 @@ Compute the root locus of the SISO LTISystem `P` with a negative feedback loop a
 `roots` is a complex matrix containig the poles trajectories of the closed-loop `1+k⋅G(s)` as a function of `k`, `Z` contains the zeros of the open-loop system `G(s)` and `K` the values of the feedback gain.
 """
 function rlocus(P; K=500)
-    K = K isa Number ? range(1e-6,stop=K,length=10000) : K
     Z = tzeros(P)
     roots, K = getpoles(P,K)
     roots, Z, K
--- test/test_rootlocus.jl
+++ test/test_rootlocus.jl
@@ -1,8 +1,21 @@
 using Plots
+
 tf2 = tf([1/5,2],[1,1,1])
+rts, Z, K = rlocus(tf2)
+rts2, Z2, _ = rlocus(tf2; K=K)
+@test size(rts) == size(rts2)
+
+P, Q = numpoly(tf2)[], denpoly(tf2)[]
+for (k, rs, rs2) = zip(eachrow(K), eachrow(rts), eachrow(rts2))
+    # test that entries are solutions
+    for r in rs
+        @test isapprox((k[1]*P+Q)(r), 0, atol=1e-12)
+    end
+    @test isapprox(rs, rs2)
+end
 rlocusplot(tf2)
 
-# https://github.com/JuliaControl/ControlSystemsBase.jl/issues/740
+# https://github.com/JuliaControl/ControlSystems.jl/issues/740
 Nroots = -1.0 .+  [-1.732050807568877im, 1.732050807568877im]
 Droots =  [0.0, -4.0, -6.0, -0.7 - 0.7141428428542851im, -0.7 + 0.7141428428542851im]
 G = zpk(Nroots, Droots, 1.0)
