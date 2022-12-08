0bcab804457c716fb8a0a42f785f19432db881d0
Fredrik Bagge Carlson <baggepinnen@gmail.com>
2022-12-08 19:31:15 +0100

    add more docs for sensitivity

--- lib/ControlSystemsBase/src/sensitivity_functions.jl
+++ lib/ControlSystemsBase/src/sensitivity_functions.jl
@@ -22,6 +22,13 @@ d₁────+──┴──►  P  ├─────┬──►e₄
 
 """
 See [`output_sensitivity`](@ref)
+
+The output [sensitivity function](https://en.wikipedia.org/wiki/Sensitivity_(control_systems)) ``S_o = (I + PC)^{-1}`` is the transfer function from a reference input to control error, while the input sensitivity function ``S_i = (I + CP)^{-1}`` is the transfer function from a disturbance at the plant input to the total plant input. For SISO systems, input and output sensitivity functions are equal. In general, we want to minimize the sensitivity function to improve robustness and performance, but pracitcal constraints always cause the sensitivity function to tend to 1 for high frequencies. A robust design minimizes the peak of the sensitivity function, ``M_S``. The peak magnitude of ``S`` is the inverse of the distance between the open-loop Nyquist curve and the critical point -1. Upper bounding the sensitivity peak ``M_S`` gives lower-bounds on phase and gain margins according to
+```math
+ϕ_m ≥ 2\\text{sin}^{-1}(\\frac{1}{2M_S}), g_m ≥ \\frac{M_S}{M_S-1}
+```
+Generally, bounding ``M_S`` is a better objective than looking at gain and phase margins due to the possibility of combined gain and pahse variations, which may lead to poor robustness despite large gain and pahse margins.
+
 $sensdoc
 """
 function sensitivity(args...)# Sensitivity function
