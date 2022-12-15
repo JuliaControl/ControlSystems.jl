543f973682a51586f001badd86e31ff406362fab
Fredrik Bagge Carlson <baggepinnen@gmail.com>
2022-12-15 14:06:26 +0100

    provide more intuitive calling syntax for `rlocus`

--- src/root_locus.jl
+++ src/root_locus.jl
@@ -70,18 +70,19 @@ end
 
 
 """
-    roots, Z, K = rlocus(P::LTISystem; K)
+    roots, Z, K = rlocus(P::LTISystem, K = 500)
 
 Compute the root locus of the SISO LTISystem `P` with a negative feedback loop and feedback gains between 0 and `K`. `rlocus` will use an adaptive step-size algorithm to determine the values of the feedback gains used to generate the plot.
 
 `roots` is a complex matrix containig the poles trajectories of the closed-loop `1+k⋅G(s)` as a function of `k`, `Z` contains the zeros of the open-loop system `G(s)` and `K` the values of the feedback gain.
 """
-function rlocus(P; K=500)
+function rlocus(P, K)
     Z = tzeros(P)
     roots, K = getpoles(P,K)
     roots, Z, K
 end
 
+rlocus(P; K=500) = rlocus(P, K)
 
 """
     rlocusplot(P::LTISystem; K)
