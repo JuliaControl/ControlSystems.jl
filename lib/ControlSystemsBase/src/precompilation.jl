c400e1db38e86314be5f8904e6c898427c076f56
Fredrik Bagge Carlson <baggepinnen@gmail.com>
2023-05-15 09:07:27 +0200

    add precompilation workload

---
+++ lib/ControlSystemsBase/src/precompilation.jl
@@ -0,0 +1,31 @@
+using PrecompileTools
+
+
+PrecompileTools.@setup_workload begin
+    P = ssrand(1,1,2, proper=true)
+    PrecompileTools.@compile_workload begin
+        Pd = c2d(P, 0.1)
+        for P in (P, Pd)
+            bode(P)
+            # nyquist(P)
+            step(P)
+            feedback(P)
+            feedback(P, P)
+            minreal(P)
+            balance_statespace(P)
+            P*P
+            P+P
+            2.0*P
+            # hinfnorm(P)
+            poles(P)
+            tzeros(P)
+        end
+        G = tf(1.0, [1.0, 1])
+        ss(G)
+        G = tf(1.0, [1.0, 1], 1)
+        ss(G)
+
+        # Pdel = P*delay(1.0)
+        # pade(Pdel, 2)
+    end
+end
