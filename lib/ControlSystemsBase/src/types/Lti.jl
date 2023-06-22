0455363af5a32705ba382e5e177e1124c53062d4
Fredrik Bagge Carlson <baggepinnen@gmail.com>
2023-06-22 07:14:26 +0200

    fix typo

--- lib/ControlSystemsBase/src/types/Lti.jl
+++ lib/ControlSystemsBase/src/types/Lti.jl
@@ -104,7 +104,7 @@ system_name(::LTISystem) = ""
     input_names(P)
     input_names(P, i)
 
-Get a vector of strings with the names of the inputs of `P`, or the `i`:th name if and index is given.
+Get a vector of strings with the names of the inputs of `P`, or the `i`:th name if an index is given.
 """
 input_names(P::LTISystem; kwargs...) = [input_names(P, i; kwargs...) for i in 1:ninputs(P)]
 function input_names(P::LTISystem, i; default = "u")
@@ -117,7 +117,7 @@ end
     output_names(P)
     output_names(P, i)
 
-Get a vector of strings with the names of the outputs of `P`, or the `i`:th name if and index is given.
+Get a vector of strings with the names of the outputs of `P`, or the `i`:th name if an index is given.
 """
 output_names(P::LTISystem; kwargs...) = [output_names(P, i; kwargs...) for i in 1:noutputs(P)]
 function output_names(P::LTISystem, i; default = "y")
@@ -130,7 +130,7 @@ end
     state_names(P)
     state_names(P, i)
 
-Get a vector of strings with the names of the states of `P`, or the `i`:th name if and index is given.
+Get a vector of strings with the names of the states of `P`, or the `i`:th name if an index is given.
 """
 state_names(P::LTISystem; kwargs...) = [state_names(P, i; kwargs...) for i in 1:nstates(P)]
 function state_names(P::LTISystem, i; default = "x")
