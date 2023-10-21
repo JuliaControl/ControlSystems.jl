002a315d414e5ac2ba264e939336f8b2b1770563
Fredrik Bagge Carlson <baggepinnen@gmail.com>
2023-10-21 09:16:13 +0200

    add method to `DSP.filt` and `filtfilt`

--- lib/ControlSystemsBase/src/dsp.jl
+++ lib/ControlSystemsBase/src/dsp.jl
@@ -37,3 +37,29 @@ function zpk(p::DSP.ZeroPoleGain, h::Real)
     z,p,k = p.z, p.p, p.k
     zpk(z, p, k, h)
 end
+
+"""
+    DSP.filt(P::ControlSystemsBase.TransferFunction, u)
+
+Use a transfer function `P` to filter a signal `u`. This is equivalent to `lsim(P, u').y[:]`, but may be more efficient for single-input, single-output systems when the state sequence is not needed.
+"""
+function DSP.filt(P::ControlSystemsBase.TransferFunction, u, args...)
+    issiso(P) || error("Only single-input, single-output systems are supported in filt, call lsim instead.")
+    b, a = numvec(P)[], denvec(P)[]
+    nb, na = length(b), length(a)
+    if nb <= na
+        b = [zeros(na - nb); b]
+    end
+    DSP.filt(b, a, u, args...)
+end
+
+
+function DSP.filtfilt(P::ControlSystemsBase.TransferFunction, u, args...)
+    issiso(P) || error("Only single-input, single-output systems are supported in filtfilt.")
+    b, a = numvec(P)[], denvec(P)[]
+    nb, na = length(b), length(a)
+    if nb <= na
+        b = [zeros(na - nb); b]
+    end
+    DSP.filtfilt(b, a, u, args...)
+end
