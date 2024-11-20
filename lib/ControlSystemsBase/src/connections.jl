b39d0e8697d8396a2235c77860594d7bff7ee2b1
Fredrik Bagge Carlson <baggepinnen@gmail.com>
2024-11-20 11:09:14 +0100

    add keyword to docstring

--- lib/ControlSystemsBase/src/connections.jl
+++ lib/ControlSystemsBase/src/connections.jl
@@ -183,6 +183,8 @@ y  &= [C; C_2]x + [D; D_2]u \\\\
 \\end{aligned}
 ```
 If `C2` is an integer it will be interpreted as an index and an output matrix containing a single 1 at the specified index will be used.
+
+When called with `C2 = I(sys.nx)`, this function is in some settings known to as `augstate`.
 """
 function add_output(sys::AbstractStateSpace, C2::AbstractArray, D2=0)
     T = promote_type(numeric_type(sys), eltype(C2), eltype(D2))
