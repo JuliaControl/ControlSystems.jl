40a14ebf06c00966cc4fd10a353c94a1458cf3e6
Fredrik Bagge Carlson <baggepinnen@gmail.com>
2025-01-08 11:02:05 +0100

    Add function for stable/unstable decomposition (#947)
    * Add function for stable/unstable decomposition
    
    * fix another DSP deprecation
    
    * fix
    
    * test tweak

--- lib/ControlSystemsBase/src/analysis.jl
+++ lib/ControlSystemsBase/src/analysis.jl
@@ -1,7 +1,10 @@
 """
     poles(sys)
 
-Compute the poles of system `sys`."""
+Compute the poles of system `sys`.
+
+Note: Poles with multiplicity `n > 1` may suffer numerical inaccuracies on the order `eps(numeric_type(sys))^(1/n)`, i.e., a double pole in a system with `Float64` coefficients may be computed with an error of about `√(eps(Float64)) ≈ 1.5e-8`.
+"""
 poles(sys::AbstractStateSpace) = eigvalsnosort(sys.A)
 
 # Seems to have a lot of rounding problems if we run the full thing with sisorational,
