208ca926894e6e2e71b2426b4eb00eb4afb84564
Fredrik Bagge Carlson <baggepinnen@gmail.com>
2025-02-14 07:54:08 +0100

    improve tf2ss conversion
    by using MatrixPencils, which offers possibility of constructing a minimal realization. This option is _much_ faster than the old approach `minreal(ss(P))` for large systems

--- lib/ControlSystemsBase/src/types/StateSpace.jl
+++ lib/ControlSystemsBase/src/types/StateSpace.jl
@@ -128,6 +128,7 @@ StateSpace(sys::LTISystem; kwargs...) = convert(StateSpace, sys; kwargs...)
 """
     sys = ss(A, B, C, D)      # Continuous
     sys = ss(A, B, C, D, Ts)  # Discrete
+    sys = ss(P::TransferFunction; balance=true, minimal=false) # Convert TransferFunction to StateSpace
 
 Create a state-space model `sys::StateSpace{TE, T}`
 with matrix element type `T` and TE is `Continuous` or `<:Discrete`.
@@ -138,7 +139,9 @@ Otherwise, this is a discrete-time model with sampling period `Ts`.
 `D` may be specified as `0` in which case a zero matrix of appropriate size is constructed automatically. 
 `sys = ss(D [, Ts])` specifies a static gain matrix `D`.
 
-To associate names with states, inputs and outputs, see [`named_ss`](https://juliacontrol.github.io/RobustAndOptimalControl.jl/dev/#Named-systems).
+When a transfer function `P` is converted to a state-space model, the user may choose whether to balance the state-space model (default=true) and/or to return a minimal realization (default=false). This conversion has more keyword argument options, see `?ControlSystemsBase.MatrixPencils.rm2ls` for additional details.
+
+To associate names with state variables, inputs and outputs, see [`named_ss`](https://juliacontrol.github.io/RobustAndOptimalControl.jl/dev/#Named-systems).
 """
 ss(args...;kwargs...) = StateSpace(args...;kwargs...)
 
